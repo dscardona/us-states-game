ef160ff94f1a3cf4bcc14b1d161d069edb191fa1
Diana Cardona <dscardona@gmail.com>
2023-08-30 18:15:18 -0400

    Add US image and set as turtle shape

--- main.py
+++ main.py
@@ -3,8 +3,12 @@ import turtle
 screen = turtle.Screen()
 screen.title("U.S. States Game")
 
+#Import image and add as turtle shape
+image = "blank_states_img.gif"
+screen.addshape(image)
 
-
+#Set turtle shape to image
+turtle.shape(image)
 
 
 screen.exitonclick()
